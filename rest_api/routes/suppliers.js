ed1fd20324fa3e55aeeb5b77f3f2cb1322f9a0a5
MichaelJimenezC <micjejim@espol.edu.ec>
2023-12-13 09:43:39 -0500

    funcional

--- rest_api/routes/suppliers.js
+++ rest_api/routes/suppliers.js
@@ -4,16 +4,19 @@ var router = express.Router();
 const { Sequelize, Op } = require('sequelize');
 const Suppliers = require('../models').suppliers;
 
+
 /* GET users listing. */
-router.get('/findAll/json', function(req, res, next) {
+router.get('/findAll/', function(req, res, next) {
     Suppliers.findAll({  
     })  
-    .then(suppliers => {  
-        res.json(suppliers);  
+    .then(data => {  
+        res.json(data);  
     })  
     .catch(error => res.status(400).send(error)) 
   });
-router.get('/findById/:id', function(req, res, next) {
+
+
+  router.get('/findById/:id', function(req, res, next) {
 
     let id = parseInt(req.params.id);
   
@@ -30,28 +33,71 @@ router.get('/findById/:id', function(req, res, next) {
     .catch(error => res.status(400).send(error)) 
   });
 
+  
+
 router.put('/update/:id', function(req, res, next) { 
-    res.send("PUT")
+      let id = parseInt(req.params.id);
+    
+    let { SupplierName, ContactName, Address,City,PostalCode,Country,Phone} = req.body;
+        
+    Suppliers.update({
+        SupplierName: SupplierName,
+        ContactName: ContactName,
+        Address: Address,
+        City: City,
+        PostalCode: PostalCode,
+        Country: Country,
+        Phone:Phone
+    },
+    {
+        where: {
+            [Op.and]: [
+                {SupplierID: id}
+              ]
+        }
+    })
+    .then(data => {  
+      res.json(data);  
+  })  
+  .catch(error => res.status(400).send(error)) 
 });  
-router.delete('/delete/:id', function(req, res, next) { 
-    res.send("DELETE")
+
+
+/* DELETE user. */
+router.delete('/delete/:id', function(req, res, next) {  
+
+    let id = parseInt(req.params.id);
+        
+    Suppliers.destroy({
+      where: { 
+        [Op.and]: [
+          {SupplierID: id}
+        ]
+      }
+    })
+    .then(data => {  
+    res.json(data);  
+})  
+.catch(error => res.status(400).send(error)) 
 });
+
+
 router.post('/save', function(req, res, next) { 
-    let {SupplierName, ContactName, Address, City, PostalCode, Country, Phone} = req.body;
-          
-      Suppliers.create({
-          SupplierName: SupplierName, 
-          ContactName: ContactName, 
-          Address: Address, 
-          City: City, 
-          PostalCode: PostalCode, 
-          Country: Country, 
-          Phone: Phone
-      })
-      .then(data => {  
-        res.json(data);  
-    })  
-    .catch(error => res.status(400).send(error)) 
-  });
+  let {SupplierName, ContactName, Address, City, PostalCode, Country, Phone} = req.body;
+        
+    Suppliers.create({
+        SupplierName: SupplierName, 
+        ContactName: ContactName, 
+        Address: Address, 
+        City: City, 
+        PostalCode: PostalCode, 
+        Country: Country, 
+        Phone: Phone
+    })
+    .then(data => {  
+      res.json(data);  
+  })  
+  .catch(error => res.status(400).send(error)) 
+});
 
 module.exports = router;
